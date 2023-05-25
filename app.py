8f5faaa703d806ac4894f6397f291a740bc0b7e3
Drishti Singh <drishtisingh0925@gmail.com>
2023-05-25 12:15:05 +0530

    Updated

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, File, UploadFile
 import json
+from datetime import datetime,date
 
 app=FastAPI(debug=True)
 
@@ -13,16 +14,18 @@ async def upload(f: UploadFile= File()):
 
 @app.get('/range/{start}/{end}')
 
-async def xrange(start: int, end: int):
+async def xrange(start: date, end: date):
     x= fileData["X"]
     y= fileData["Y"]
     X_list=[]
     Y_list=[]
-    for i in range(0, len(x)):
-        if (start<x[i]<end):
+    for i in range(len(x)):
+        datetime_obj = datetime.strptime(x[i], "%Y-%m-%dT%H:%M:%S").date()  # Convert string to date object
+        date_obj = datetime_obj.date()
+        if start < date_obj < end:
             X_list.append(x[i])
             Y_list.append(y[i])
-        
+           
     return {"X":X_list, "Y":Y_list}
 
 
